f6b1f1334b6b25c0f8c807e7588c7fd3927a04c5
Hardik Garg <hardik12c@gmail.com>
2023-08-21 01:16:10 +0530

    Login button added

--- todo-app/components/Clients.tsx
+++ todo-app/components/Clients.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import UserStore from "@/Zustand/userStore";
+
 export const LogoutBtn = () => {
+  const User = UserStore((state: any) => state.User);
   const logoutHandler = () => {
     alert("Logout");
   };
-  return (
+  return User._id ? (
     <button className="btn" onClick={logoutHandler}>
       Logout
     </button>
+  ) : (
+    <button className="btn">Login</button>
   );
 };
