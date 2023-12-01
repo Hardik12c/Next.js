f03114716804a5d99a999128ed805deb39e980d5
Hardik Garg <hardik12c@gmail.com>
2023-12-01 10:12:35 +0530

    some minor changes

--- todo-app/app/layout.tsx
+++ todo-app/app/layout.tsx
@@ -6,10 +6,7 @@ import UserStore from "@/Zustand/userStore";
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
-export const metadata = {
-  title: "Todo App",
-  description: "This is a Todo App Project made for Next.js series",
-};
+
 export default function RootLayout({
   children,
 }: {
